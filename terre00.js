9da5602a76309601114f080b095cb0a664f5d6a9
antoineabln <johndoe@example.com>
2022-12-06 17:30:53 +0100

    ajout de fichier

---
+++ terre00.js
@@ -0,0 +1,14 @@
+let tabAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+function alphabet(){
+  
+  let alpha = "";
+  
+  for(let i = 0; i<tabAlphabet.length; i++){
+    
+     console.log(tabAlphabet[i]);
+  }
+                           
+}
+
+alphabet(tabAlphabet);
