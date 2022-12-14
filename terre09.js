43f1b32dfc02f3509d2dfa13262e9e467ae9b313
antoineabln <johndoe@example.com>
2022-12-14 07:31:12 +0100

    affiche la racine carrée d’un entier positif

---
+++ terre09.js
@@ -0,0 +1,17 @@
+let nbr = 9;
+
+function racineCarre(){
+  
+  if(nbr < 0){
+    
+    console.log("erreur");
+  }
+  else{
+  
+  console.log(Math. sqrt(nbr));
+    
+  }
+  
+}
+
+racineCarre(nbr);
