0c840467534ad6818a9cd9dab1ad24371922cbe6
antoineabln <johndoe@example.com>
2022-12-19 06:52:59 +0100

    prend en paramètre trois entiers et affiche la valeur du milieu

---
+++ terre12.js
@@ -0,0 +1,26 @@
+let tab = [2,2,2];
+
+function milieu(){
+  
+  if(tab[0] <tab[1] && tab[1]<tab[2]){
+    
+    return tab[1];
+    
+  }else if(tab[1] <tab[0] && tab[0]<tab[2]){
+    
+    return tab[0];
+    
+  }
+  
+  else if(tab[1] == tab[0] && tab[0]== tab[2]){
+    
+    return "erreur";}
+    
+  else{
+    
+    return tab[2];
+    
+  }
+}
+
+console.log(milieu(tab));
